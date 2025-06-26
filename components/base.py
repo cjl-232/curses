c9bfa668910297e41e3512debae7af6a91c0b5db
Chris Linnell <chrislinnell232@gmail.com>
2025-06-26 16:54:03 +0100

    Replaced dimensions with relative proportions

--- components/base.py
+++ components/base.py
@@ -1,50 +1,52 @@
 import abc
 import curses
+import math
 
 class ComponentWindow(metaclass=abc.ABCMeta):
     def __init__(
             self,
-            height: int,
-            width: int,
-            top: int = 0,
-            left: int = 0,
-            bordered: bool = True,
-            focusable: bool = True
+            stdscr: curses.window,
+            height: float,
+            width: float,
+            top: float,
+            left: float,
+            focusable: bool = True,
         ):
-        self._window = curses.newwin(height, width, top, left)
-        self._bordered = bordered
-        self._focused = False
+        self._stdscr = stdscr
+        self._height = height
+        self._width = width
+        self._top = top
+        self._left = left
         self._focusable = focusable
+        self.reset_window()
 
     @abc.abstractmethod
-    def draw(self):
+    def draw(self, focused: bool):
         """
         Draw the window on the stdscr terminal.
 
-        This function should begin by calling self._window.erase, unless it
-        is certain nothing will need to be erased. It should always draw a
-        border if self._bordered == True. It should always conclude by calling
-        self._window.refresh.
+        This function should begin by calling self._window.erase unless it
+        is certain nothing will need to be erased. It should always call
+        self._draw_border, and should always end with a refresh.
         """
 
     @abc.abstractmethod
     def handle_key(self, key: int):
-        """Handle key presses when this window is active."""
+        """Handle key presses when this window is focused."""
 
     @property
     def is_focusable(self):
         return self._focusable
 
-    def resize(self, height: int, width: int, top: int = 0, left: int = 0):
-        self._window = curses.newwin(height, width, top, left)
+    def reset_window(self):
+        screen_height, screen_width = self._stdscr.getmaxyx()
+        self._window = curses.newwin(
+            math.floor(self._height * screen_height),
+            math.floor(self._width * screen_width),
+            math.floor(self._top * screen_height),
+            math.floor(self._left * screen_width),
+        )
 
     def set_focused(self, focused: bool):
-        """Set whether the window is focused. Redraw borders if relevant."""
-        if focused != self._focused and self._bordered:
-            if focused:
-                self._window.attron(curses.A_BOLD)
-                self._window.box()
-                self._window.attroff(curses.A_BOLD)
-            else:
-                self._window.box()
+        """Set whether the window is focused."""
         self._focused = focused
