80452d8bba0fe368299ac61ce529ad9adb82b2d1
Chris Linnell <chrislinnell232@gmail.com>
2025-06-27 00:41:58 +0100

    Testing menu

--- body.py
+++ body.py
@@ -1,6 +1,9 @@
+# Need to work out combinations (100% - 200px)
+
 import curses
 
 from components.base import ComponentWindow, MeasurementUnit
+from components.menus import PaginatedMenu
 
 class Example(ComponentWindow):
 
@@ -13,15 +16,35 @@ class Example(ComponentWindow):
     def handle_key(self, key: int):
         """Handle key presses when this window is active."""
 
-
+class Menu(PaginatedMenu):
+    def __init__(self, items: list[str], stdscr: curses.window, height: tuple[int | float, MeasurementUnit], width: tuple[int | float, MeasurementUnit], top: tuple[int | float, MeasurementUnit], left: tuple[int | float, MeasurementUnit], title: str | None = None, focusable: bool = True):
+        super().__init__(items, stdscr, height, width, top, left, title, focusable)
     
+    def handle_key(self, key: int):
+        match key:
+            case curses.KEY_UP:
+                self.cursor_index -= 1
+                if self.cursor_index < 0:
+                    self.cursor_index = len(self.items) - 1
+                self.draw(focused=True)
+            case curses.KEY_DOWN:
+                self.cursor_index += 1
+                if self.cursor_index >= len(self.items):
+                    self.cursor_index = 0
+                self.draw(focused=True)
+            case _:
+                pass
+
+
+from secrets import token_hex
 class Body:
     def __init__(self, stdscr: curses.window):
         self.component_index = 0
         self.components: list[ComponentWindow] = [
-            Example(
+            Menu(
+                items=[token_hex(12) for _ in range(50)],
                 stdscr=stdscr,
-                height=(1.0, MeasurementUnit.PERCENTAGE),
+                height=(0.8, MeasurementUnit.PERCENTAGE),
                 width=(0.2, MeasurementUnit.PERCENTAGE),
                 top=(0, MeasurementUnit.PIXELS),
                 left=(0, MeasurementUnit.PIXELS),
@@ -29,7 +52,7 @@ class Body:
             ),
             Example(
                 stdscr=stdscr,
-                height=(0.9, MeasurementUnit.PERCENTAGE),
+                height=(0.8, MeasurementUnit.PERCENTAGE),
                 width=(0.8, MeasurementUnit.PERCENTAGE),
                 top=(0, MeasurementUnit.PIXELS),
                 left=(0.2, MeasurementUnit.PERCENTAGE),
@@ -37,10 +60,10 @@ class Body:
             ),
             Example(
                 stdscr=stdscr,
-                height=(0.1, MeasurementUnit.PERCENTAGE),
-                width=(0.8, MeasurementUnit.PERCENTAGE),
-                top=(0.9, MeasurementUnit.PERCENTAGE),
-                left=(0.2, MeasurementUnit.PERCENTAGE),
+                height=(0.2, MeasurementUnit.PERCENTAGE),
+                width=(1.0, MeasurementUnit.PERCENTAGE),
+                top=(0.8, MeasurementUnit.PERCENTAGE),
+                left=(0, MeasurementUnit.PIXELS),
                 title='Window 3',
             ),
         ]
@@ -49,6 +72,7 @@ class Body:
 
     def run(self, stdscr: curses.window):
         stdscr.keypad(True)
+        stdscr.nodelay(True)
         self._draw_components()
         stdscr.refresh()
         while True:
