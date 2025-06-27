668430d6a7f28c78aa9dd95797c9a1413dfefb86
Chris Linnell <chrislinnell232@gmail.com>
2025-06-28 00:28:54 +0100

    Created and tested message log

--- body.py
+++ body.py
@@ -3,8 +3,9 @@
 import curses
 
 from components.base import ComponentWindow, MeasurementUnit
-from components.contacts import ContactsMenu
+from components.contacts import ContactsMenu, get_contacts
 from components.menus import PaginatedMenu
+from components.messages import MessageLog
 
 class Example(ComponentWindow):
 
@@ -12,6 +13,8 @@ class Example(ComponentWindow):
         self._window.erase()
         self._draw_border(focused)
         self._window.refresh()
+        self.draw_required = False
+        print('Drew example obj')
 
 
     def handle_key(self, key: int):
@@ -44,15 +47,18 @@ from database.models import Base, Contact
 engine = create_engine('sqlite:///contacttest.db')
 Base.metadata.create_all(engine)
 from sqlalchemy.orm import Session
-with Session(engine) as session:
-    for _ in range(20):
-        contact = Contact(name=f'Contact #{token_hex(8)}', verification_key=urlsafe_b64encode(token_bytes(32)).decode())
-        session.add(contact)
-    session.commit()
+from database.outputs.schemas import ContactOutputSchema
+# with Session(engine) as session:
+#     for _ in range(20):
+#         contact = Contact(name=f'Contact #{token_hex(8)}', verification_key=urlsafe_b64encode(token_bytes(32)).decode())
+#         session.add(contact)
+#     session.commit()
 
 
 class Body:
     def __init__(self, stdscr: curses.window):
+        with Session(engine) as session:
+            contact = ContactOutputSchema.model_validate(session.get(Contact, 1))
         self.component_index = 0
         self.components: list[ComponentWindow] = [
             ContactsMenu(
@@ -64,13 +70,14 @@ class Body:
                 left=(0, MeasurementUnit.PIXELS),
                 title='Contacts',
             ),
-            Example(
+            MessageLog(
+                engine=engine,
+                contact=contact,
                 stdscr=stdscr,
                 height=(0.8, MeasurementUnit.PERCENTAGE),
                 width=(0.8, MeasurementUnit.PERCENTAGE),
                 top=(0, MeasurementUnit.PIXELS),
                 left=(0.2, MeasurementUnit.PERCENTAGE),
-                title='Window 2',
             ),
             Example(
                 stdscr=stdscr,
@@ -87,20 +94,27 @@ class Body:
     def run(self, stdscr: curses.window):
         stdscr.keypad(True)
         stdscr.nodelay(True)
-        self._draw_components()
+        stdscr.clear()
         stdscr.refresh()
         while True:
             self._draw_components()
             key = stdscr.getch()
             if key == 81 or key == 113: # Q
                 break
-            elif key == 9: # TAB
+            elif key == 9 or key == curses.KEY_BTAB: # TAB or SHIFT-TAB
+                self.components[self.component_index].draw_required = True
                 for _ in range(len(self.components)):
-                    self.component_index += 1
-                    if self.component_index >= len(self.components):
-                        self.component_index = 0
+                    if key == 9:
+                        self.component_index += 1
+                        if self.component_index >= len(self.components):
+                            self.component_index = 0
+                    else:
+                        self.component_index -= 1
+                        if self.component_index < 0:
+                            self.component_index = len(self.components) - 1
                     if self.components[self.component_index].is_focusable:
                         break
+                self.components[self.component_index].draw_required = True
             elif key == curses.KEY_RESIZE:
                 stdscr.erase()                
                 for component in self.components:
@@ -111,6 +125,7 @@ class Body:
 
     def _draw_components(self):
         for index, component in enumerate(self.components):
-            component.draw(index == self.component_index)
+            if component.draw_required:
+                component.draw(index == self.component_index)
 
 curses.wrapper(lambda stdscr: Body(stdscr).run(stdscr))
--- components/messages.py
+++ components/messages.py
@@ -1,16 +1,151 @@
 import curses
+import textwrap
 
-from components.base import Component
-from database.outputs.schemas import ContactOutputSchema
+from sqlalchemy import Engine, select
+from sqlalchemy.orm import Session
 
-class MessagesScreen(Component):
-    def __init__(self, contact: ContactOutputSchema):
-        self.contact = contact
+from components.base import ComponentWindow, Measurement
+from database.models import Message, MessageType
+from database.outputs.schemas import ContactOutputSchema, MessageOutputSchema
+from settings import settings
 
-    def run(self, stdscr: curses.window):
-        stdscr.nodelay(False)
-        stdscr.clear()
-        stdscr.addstr(stdscr.getmaxyx()[0] - 1, 0, f'This will be used for messaging {self.contact.name}. Please press enter to go back.')
-        stdscr.refresh()
-        while stdscr.getch() != 10:
-            pass
+class MessageLog(ComponentWindow):
+    def __init__(
+            self,
+            engine: Engine,
+            contact: ContactOutputSchema | None,
+            stdscr: curses.window,
+            height: Measurement,
+            width: Measurement,
+            top: Measurement,
+            left: Measurement,
+            focusable: bool = True,
+        ):
+        self._engine = engine
+        self._contact = contact
+        self._scroll_index: int = 0 # Scroll upwards
+        self._message_lines: list[tuple[str, bool]] = list()
+        self._loaded_nonces: list[str] = list()
+        super().__init__(
+            stdscr=stdscr,
+            height=height,
+            width=width,
+            top=top,
+            left=left,
+            title=contact.name if contact is not None else None,
+            focusable=focusable,
+        )
+
+    def draw(self, focused: bool):
+        self._window.erase()
+        self._draw_border(focused)
+        height, width = (x - 2 for x in self._window.getmaxyx())
+        if height <= 0 or width <= 0:
+            self._window.refresh()
+            self.draw_required = False
+            return
+        if self._scroll_index == 0:
+            visible_lines = self._message_lines[-height:]
+        else:
+            start = 0 - height - self._scroll_index
+            stop = -self._scroll_index
+            visible_lines = self._message_lines[start:stop]
+        for index, (line, header) in enumerate(visible_lines):
+            if header:
+                self._window.attron(curses.A_BOLD)
+                self._window.addnstr(index + 1, 1, line, width)
+                self._window.attroff(curses.A_BOLD)
+            else:
+                self._window.addnstr(index + 1, 1, line, width)
+        self._window.refresh()
+        self.draw_required = False
+
+    def handle_key(self, key: int):
+        height = self._window.getmaxyx()[0] - 2
+        if key in settings.key_bindings.up_key_set:
+            self._scroll_index += 1
+            if self._scroll_index >= len(self._message_lines) - height:
+                self._scroll_index = len(self._message_lines) - 1 - height
+            self.draw_required = True
+        elif key in settings.key_bindings.down_key_set:
+            self._scroll_index -= 1
+            if self._scroll_index < 0:
+                self._scroll_index = 0
+            self.draw_required = True
+        elif key == curses.KEY_F5:
+            self.update()
+        elif key == 281: # SHIFT-F5
+            self._refresh()
+
+    def reset_window(self):
+        super().reset_window()
+        self._refresh()
+
+    def set_contact(self, contact: ContactOutputSchema | None):
+        if self._contact is None and contact is None:
+            return
+        elif self._contact is not None and contact is not None:
+            if self._contact.id == contact.id:
+                return
+        self._contact = contact
+        if contact is not None:
+            self.title = contact.name
+        else:
+            self.title = None
+        self._refresh()
+        self.draw_required = True
+
+    def update(self):
+        self._load_messages()
+
+    def _format_message(
+            self,
+            message: MessageOutputSchema
+        ) -> list[tuple[str, bool]]:
+        width = self._window.getmaxyx()[1] - 2
+        if width <= 0:
+            return []
+        wrapped_text = textwrap.wrap(message.text, width)
+        if wrapped_text:
+            assert self._contact is not None
+            if message.message_type == MessageType.received:
+                name = f'{self._contact.name}:'
+            else:
+                name = 'You:'
+            header = (
+                f'{name: <{max(0, width - 16)}}'
+                f'{message.timestamp.strftime('%Y-%m-%d %H:%M')}'
+            )
+            result = [('', False), (header[:width], True)]
+            for line in wrapped_text:
+                result.append((line, False))
+            return result
+        return []
+
+    def _load_messages(self):
+        if self._contact is None:
+            return
+        width = self._window.getmaxyx()[1] - 2
+        if width <= 0:
+            return
+        with Session(self._engine) as session:
+            query = (
+                select(Message)
+                .where(Message.contact_id == self._contact.id)
+                .where(~Message.nonce.in_(self._loaded_nonces))
+                .order_by(Message.timestamp)
+            )
+            for obj in session.scalars(query):
+                output = MessageOutputSchema.model_validate(obj)
+                for line in self._format_message(output):
+                    if self._scroll_index > 0:
+                        self._scroll_index += 1
+                    self._message_lines.append(line)
+                self._loaded_nonces.append(output.nonce)
+                self.draw_required = True
+
+    def _refresh(self):
+        self._loaded_nonces.clear()
+        self._message_lines.clear()
+        self._scroll_index = 0
+        self._load_messages()
