481f9b97269e9650cd8c3272c8a0f3262e6408fd
Chris Linnell <chrislinnell232@gmail.com>
2025-07-13 16:27:41 +0100

    Removed unused operations

--- database/operations.py
+++ database/operations.py
@@ -1,11 +1,8 @@
 from base64 import urlsafe_b64encode
-from functools import cache
+from functools import lru_cache
 
 from cryptography.fernet import Fernet
-from cryptography.hazmat.primitives.asymmetric.x25519 import (
-    X25519PrivateKey,
-    X25519PublicKey,
-)
+from cryptography.hazmat.primitives.asymmetric.x25519 import X25519PrivateKey
 from sqlalchemy import Engine, exists, select
 from sqlalchemy.orm import Session
 
@@ -24,11 +21,12 @@ from database.schemas.inputs import (
 )
 from database.schemas.outputs import (
     ContactOutputSchema,
-    SentKeyOutputSchema,
     ReceivedKeyOutputSchema,
+    SentKeyOutputSchema,
 )
-from exceptions import MissingFernetKey
 from server.schemas.responses import (
+    FetchResponseExchangeKey,
+    FetchResponseMessage,
     FetchResponseSchema,
     PostMessageResponseSchema
 )
@@ -37,6 +35,7 @@ def add_contact(engine: Engine, contact: ContactInputSchema):
     with Session(engine) as session:
         session.add(Contact(**contact.model_dump()))
         session.commit()
+
 
 def get_contact(
         engine: Engine,
@@ -52,7 +51,12 @@ def get_contact(
         if contact is not None:
             return ContactOutputSchema.model_validate(contact)
         return None
-            
+
+
+def get_contact_keys(engine: Engine) -> list[str]:
+    with Session(engine) as session:
+        return list(session.scalars(select(Contact.verification_key)))
+
 
 def get_contacts(engine: Engine) -> list[ContactOutputSchema]:
     query = (
@@ -62,8 +66,10 @@ def get_contacts(engine: Engine) -> list[ContactOutputSchema]:
     with Session(engine) as session:
         contacts = session.scalars(query)
         return [ContactOutputSchema.model_validate(x) for x in contacts]
-    
+
+
 def get_unmatched_keys(engine: Engine) -> list[ReceivedKeyOutputSchema]:
+    """Return all received keys that have not yet been responded to."""
     query = (
         select(ReceivedExchangeKey)
         .where(ReceivedExchangeKey.matched == False)
@@ -71,148 +77,131 @@ def get_unmatched_keys(engine: Engine) -> list[ReceivedKeyOutputSchema]:
     with Session(engine) as session:
         keys = session.scalars(query)
         return [ReceivedKeyOutputSchema.model_validate(x) for x in keys]
-    
-def get_fernet_key(engine: Engine, contact: ContactOutputSchema) -> Fernet:
-    """Retrieve the latest Fernet key available for a contact."""
-    query = (
-        select(FernetKey.encoded_bytes)
-        .where(FernetKey.contact_id == contact.id)
-        .order_by(FernetKey.timestamp.desc())
-    )
-    with Session(engine) as session:
-        encoded_bytes = session.scalar(query)
-        if encoded_bytes is None:
-            msg = f'No shared key exists for {contact.name}.'
-            raise MissingFernetKey(msg)
-        return Fernet(encoded_bytes)
 
-    
-def store_message(
-        engine: Engine,
-        contact: ContactOutputSchema,
-        plaintext: str,
-    ):
-    """Encrypt a message and prepare it for posting."""
 
-def store_fetched_data(engine: Engine, response: FetchResponseSchema):
-    def exchange(private_key: X25519PrivateKey, public_key: X25519PublicKey):
-        return urlsafe_b64encode(private_key.exchange(public_key)).decode()
-    # Cached functions are used for data retrieval.
-    @cache
-    def get_contact_id(key: str) -> int | None:
-        query = select(Contact.id).where(Contact.verification_key == key)
-        with Session(engine) as session:
-            return session.scalar(query)
-    @cache
-    def get_fernet_keys(contact_id: int) -> list[Fernet]:
-        query = (
-            select(FernetKey.encoded_bytes)
-            .where(FernetKey.contact_id == contact_id)
-            .order_by(FernetKey.timestamp.desc())
-        )
-        with Session(engine) as session:
-            return [Fernet(x) for x in session.scalars(query)]
-    @cache
-    def received_key_exists(b64_key: str) -> bool:
-        query = (
-            exists()
-            .where(ReceivedExchangeKey.encoded_bytes == b64_key)
-            .select()
-        )
-        with Session(engine) as session:
-            return bool(session.scalar(query))
-    def nonce_exists(nonce: str) -> bool:
-        query = exists().where(Message.nonce == nonce).select()
-        with Session(engine) as session:
-            return bool(session.scalar(query))
-        
-    # Iterate over fetched messages and keys.
-    with Session(engine) as session:
-        
-        # Sent key retrieval cannot occur outside the session.
-        def get_sent_key(b64_key: str) -> SentKeyOutputSchema | None:
-            query = (
-                select(SentExchangeKey)
-                .where(SentExchangeKey.encoded_public_bytes == b64_key)
+@lru_cache(maxsize=256)
+def _get_contact_from_key(
+    session: Session,
+    public_key: str,
+) -> ContactOutputSchema | None:
+    """Internal function to identify contacts from response data."""
+    contact = session.scalar((
+        select(Contact)
+        .where(Contact.verification_key == public_key)
+    ))
+    if contact is not None:
+        return ContactOutputSchema.model_validate(contact)
+    return None
+
+
+def _get_initial_key(
+    session: Session,
+    encoded_public_bytes: str,
+) -> SentKeyOutputSchema | None:
+    """Identifies a previously sent key from response data. Not cacheable."""
+    sent_key = session.scalar((
+        select(SentExchangeKey)
+        .where(SentExchangeKey.encoded_public_bytes == encoded_public_bytes)
+    ))
+    if sent_key is not None:
+        return SentKeyOutputSchema.model_validate(sent_key)
+    return None
+
+
+def _received_key_exists(session: Session, key: str) -> bool:
+    query = exists().where(ReceivedExchangeKey.encoded_bytes == key).select()
+    return bool(session.scalar(query))
+
+
+def _received_message_exists(session: Session, nonce: str) -> bool:
+    query = exists().where(Message.nonce == nonce).select()
+    return bool(session.scalar(query))
+
+
+def _handle_exchange_key_element(
+        session: Session,
+        element: FetchResponseExchangeKey,
+    ) -> None:
+    if not element.is_valid:
+        return
+    elif _received_key_exists(session, element.exchange_key_b64):
+        return
+    contact = _get_contact_from_key(session, element.sender_key_b64)
+    if contact is None:
+        return
+    if element.initial_key_b64 is not None:
+        initial_key = _get_initial_key(session, element.initial_key_b64)
+        if initial_key is None or initial_key.contact.id != contact.id:
+            return
+        shared_secret = initial_key.private_key.exchange(element.exchange_key)
+        session.add(
+            FernetKey(
+                contact_id=contact.id,
+                encoded_bytes=shared_secret,
+                timestamp=element.timestamp,
             )
-            obj = session.scalar(query)
-            if obj is not None:
-                return SentKeyOutputSchema.model_validate(obj)
-            else:
-                return None
-                
-        for message in response.data.messages:
-            # Check each message is valid, new, and from a registered contact.
-            if not message.is_valid:
-                continue
-            elif get_contact_id(message.sender_public_key_b64) is None:
-                continue
-            elif nonce_exists(message.nonce):
-                continue
-            contact_id = get_contact_id(message.sender_public_key_b64)
-            # Attempt to decrypt each message.
-            text = ''
-            for fernet_key in get_fernet_keys(contact_id):
-                try:
-                    text = fernet_key.decrypt(message.encrypted_text).decode()
-                    break
-                except Exception:
-                    pass
-            if not text:
-                continue
-            # Add decrypted messages to the database.
-            input = MessageInputSchema.model_validate({
-                'text': text,
-                'contact_id': contact_id,
-                'message_type': MessageType.RECEIVED,
-                'timestamp': message.timestamp,
-                'nonce': message.nonce,
-            })
-            session.add(Message(**input.model_dump()))
-        
-        for exchange_key in response.data.exchange_keys:
-            if not exchange_key.is_valid:
-                continue
-            elif get_contact_id(exchange_key.sender_public_key_b64) is None:
-                continue
-            elif received_key_exists(exchange_key.received_exchange_key_b64):
-                continue
-            contact_id = get_contact_id(exchange_key.sender_public_key_b64)
-            if exchange_key.initial_exchange_key_b64 is not None:
-                sent_key = get_sent_key(exchange_key.initial_exchange_key_b64)
-                if sent_key is None:
-                    continue
-                elif sent_key.contact.id != contact_id:
-                    continue
-                shared_secret = exchange(
-                    sent_key.private_key,
-                    exchange_key.received_exchange_key,
-                )
-                session.add(
-                    FernetKey(
-                        contact_id=contact_id,
-                        encoded_bytes=shared_secret,
-                        timestamp=exchange_key.timestamp,
-                    )
-                )
-                session.add(
-                    ReceivedExchangeKey(
-                        encoded_bytes=exchange_key.received_exchange_key_b64,
-                        matched=True,
-                        contact_id=contact_id,
-                    ),
-                )
-                session.delete(session.get(SentExchangeKey, sent_key.id))
-            else:
-                session.add(
-                    ReceivedExchangeKey(
-                        encoded_bytes=exchange_key.received_exchange_key_b64,
-                        matched=False,  # To be matched later
-                        contact_id=contact_id,
-                    ),
-                )
+        )
+        session.add(
+            ReceivedExchangeKey(
+                encoded_bytes=element.exchange_key_b64,
+                matched=True,
+                contact_id=contact.id,
+            ),
+        )
+        session.delete(session.get(SentExchangeKey, initial_key.id))
+    else:
+        session.add(
+            ReceivedExchangeKey(
+                encoded_bytes=element.exchange_key_b64,
+                matched=False,
+                contact_id=contact.id,
+            ),
+        )
 
+
+def _handle_message_element(
+        session: Session,
+        element: FetchResponseMessage,
+    ) -> None:
+    if not element.is_valid:
+        return
+    elif _received_message_exists(session, element.nonce):
+        return
+    contact = _get_contact_from_key(session, element.sender_key_b64)
+    if contact is None:
+        return
+    plaintext = ''
+    for fernet_key in contact.fernet_keys:
+        try:
+            plaintext = fernet_key.key.decrypt(element.encrypted_text).decode()
+            break
+        except Exception:
+            pass
+    if plaintext:
+        session.add(
+            Message(
+                text=plaintext,
+                contact_id=contact.id,
+                message_type=MessageType.RECEIVED,
+                timestamp=element.timestamp,
+                nonce=element.nonce,
+            ),
+        )
+
+
+def store_fetched_data(engine: Engine, response: FetchResponseSchema) -> None:
+    """Stores the data from a successful fetch request response."""
+    # Use an initial session for key exchange.
+    with Session(engine) as session:
+        for exchange_key in response.data.exchange_keys:
+            _handle_exchange_key_element(session, exchange_key)
         session.commit()
+    # Use a second session for messages, ensuring fernet keys are accessible.
+    with Session(engine) as session:
+        for message in response.data.messages:
+            _handle_message_element(session, message)
+        session.commit()
+
 
 def store_posted_exchange_key(
         engine: Engine,
